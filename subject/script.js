ec040ad5b18faa6312201e22c8f8efea9bd10c61
dong-hwann <dong-hwan.kim@mines-paristech.fr>
2022-02-16 20:31:02 +0100

    v1

--- subject/script.js
+++ subject/script.js
@@ -3,3 +3,32 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+/*
+Afficher dans la console la touche cliquee
+Afficher dans l'ecran
+Concatener dans l'ecran
+Trainter le cas du zero
+Faire l'addition
+Faire les autres operations
+Traiter les decimaux
+*/
+
+function printTargetContentToConsole(event) {
+    let screen = document.getElementsByClassName("calculator_display")[0];
+    if (screen.textContent == "0") {
+        screen.textContent = event.target.textContent;
+    }
+    else {
+        screen.textContent = screen.textContent + event.target.textContent;
+    }
+}
+
+window.addEventListener("load", () => {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", (event) => {
+            console.log("")
+        })
+    }
+})
